ff4e5f28423b31270bce90bd2efd9c7693ec9b5e
gusdn3477 <gusdn3477@naver.com>
2024-06-18 23:59:03 +0900

    feat: 추가된 props 바탕으로 Card, TodoList 컴포넌트 고도화

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,32 +3,50 @@ import styled from 'styled-components';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import CloseIcon from '@mui/icons-material/Close';
 import SellIcon from '@mui/icons-material/Sell';
-import { CardProps } from '@/types/Card';
 import { StyledIconWrapper } from '@/components/style';
+import { ChangeEvent } from 'react';
 
-const Card = ({
-  checked = false,
-  title,
-  date,
-  isBookMarked = false,
-  tagColor,
-}: CardProps) => {
+export interface CardProps {
+  id: number;
+  checked: boolean;
+  title: string;
+  date: string;
+  isBookMarked: boolean;
+  tagColor?: string;
+}
+
+const Card = ({ id, checked, title, date, isBookMarked }: CardProps) => {
+  const handleCheckBoxClick = (
+    id: number,
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    console.log(event.target.checked);
+  };
+
+  const handleCardDelete = (id: number) => {
+    // id를 가지고 스토어에서 해당 카드 삭제
+  };
   return (
     <StyledCard>
       <StyledCardLeft>
-        <Checkbox size="large" />
+        <Checkbox
+          onChange={event => handleCheckBoxClick(id, event)}
+          checked={checked}
+          size="large"
+        />
         <strong>{title}</strong>
-        <div>{date}</div>
+        <span>{date}</span>
       </StyledCardLeft>
+
       <StyledCardRight>
         <StyledIconWrapper>
-          <StarBorderIcon />
+          <StarBorderIcon fontSize="large" />
         </StyledIconWrapper>
         <StyledIconWrapper>
-          <SellIcon />
+          <SellIcon fontSize="large" />
         </StyledIconWrapper>
-        <StyledIconWrapper>
-          <CloseIcon />
+        <StyledIconWrapper onClick={() => handleCardDelete(id)}>
+          <CloseIcon fontSize="large" />
         </StyledIconWrapper>
       </StyledCardRight>
     </StyledCard>
@@ -49,6 +67,9 @@ const StyledCard = styled.div`
 const StyledCardLeft = styled.div`
   display: flex;
   align-items: center;
+  & > :nth-child(n + 2) {
+    margin-left: 15px;
+  }
 `;
 
 const StyledCardRight = styled.div`
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,15 +1,59 @@
 import InputField from '@/components/InputField';
 import Card from '@/components/Card';
 import styled from 'styled-components';
+import * as T from '@/types/Card';
+import { useCallback, useState, KeyboardEvent, ChangeEvent } from 'react';
+
+const data: T.Card = {
+  id: 0,
+  checked: false,
+  title: '123',
+  date: '2022-02-02',
+  isBookMarked: false,
+  tagColor: 'red',
+};
 
 const TodoList = () => {
+  const [title, setTitle] = useState('');
+  const [task, setTask] = useState('');
+
+  const handleContentsChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setTitle(event.target.value);
+    },
+    [],
+  );
+
+  const handleEnterKeyPress = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log('엔터키 클릭');
+    }
+  };
+
   return (
     <>
       <StyledHeader>
-        <InputField />
+        <InputField
+          contents={title}
+          handleContentsChange={handleContentsChange}
+          placeholder="My to-do list 06/18/2024"
+          handleEnterKeyPress={handleEnterKeyPress}
+        />
       </StyledHeader>
-      <InputField />
-      <Card />
+      <InputField
+        contents={task}
+        handleContentsChange={handleContentsChange}
+        placeholder="할 일을 입력해 주세요."
+        handleEnterKeyPress={handleEnterKeyPress}
+      />
+      <Card
+        id={data.id}
+        checked={data.checked}
+        title={data.title}
+        date={data.date}
+        isBookMarked={data.isBookMarked}
+        tagColor={data.tagColor}
+      />
     </>
   );
 };
