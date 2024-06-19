3a3b3afb1b1ce051cbd9e83387993eff693511de
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2024-06-19 20:26:47 +0900

    feat: 일정 삭제 시에 경고용으로 뜰 Dialog 구현

---
+++ src/components/ConfirmDialog.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+interface ConfirmDialogProps {
+  open: boolean;
+  handleClose: () => void;
+  handleConfirm: () => void;
+}
+
+export const ConfirmDialog = ({
+  open,
+  handleClose,
+  handleConfirm,
+}: ConfirmDialogProps) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {'일정을 삭제하시겠습니까?'}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          삭제한 일정은 복구할 수 없습니다.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>취소</Button>
+        <Button onClick={handleConfirm}>삭제하기</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
