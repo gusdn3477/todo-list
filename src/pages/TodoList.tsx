c8f0b2a306d805e536d8f44f492003e59e14a73e
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2024-06-19 14:37:10 +0900

    feat: TodoList 컴포넌트에서 제목, 할 일 작성 분기하여 함수 작성

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,45 +1,49 @@
 import InputField from '@/components/InputField';
 import styled from 'styled-components';
-import { useCallback, useState, KeyboardEvent, ChangeEvent } from 'react';
+import { KeyboardEvent, ChangeEvent } from 'react';
 import CardList from '@/components/CardList';
+import { observer, useLocalObservable } from 'mobx-react-lite';
 
-const TodoList = () => {
-  const [title, setTitle] = useState('');
-  const [task, setTask] = useState('');
+const TodoList = observer(() => {
+  const store = useLocalObservable(() => ({
+    title: '',
+    taskName: '',
+  }));
 
-  const handleContentsChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      setTitle(event.target.value);
-    },
-    [],
-  );
+  const handleContentsChange =
+    (type: 'title' | 'taskName') => (event: ChangeEvent<HTMLInputElement>) => {
+      if (type === 'title') store[type] = event.target.value;
+      else store[type] = event.target.value;
+    };
 
-  const handleEnterKeyPress = (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      console.log('엔터키 클릭');
-    }
-  };
+  const handleEnterKeyPress =
+    (type: 'title' | 'taskName') => (event: KeyboardEvent) => {
+      if (event.key === 'Enter') {
+        if (type === 'title') console.log('제목 변경');
+        else console.log('새로운 태스크 추가');
+      }
+    };
 
   return (
     <>
       <StyledHeader>
         <InputField
-          contents={title}
-          handleContentsChange={handleContentsChange}
-          placeholder="My to-do list 06/18/2024"
-          handleEnterKeyPress={handleEnterKeyPress}
+          contents={store.title}
+          handleContentsChange={handleContentsChange('title')}
+          placeholder={store.title}
+          handleEnterKeyPress={handleEnterKeyPress('title')}
         />
       </StyledHeader>
       <InputField
-        contents={task}
-        handleContentsChange={handleContentsChange}
+        contents={store.taskName}
+        handleContentsChange={handleContentsChange('taskName')}
         placeholder="할 일을 입력해 주세요."
-        handleEnterKeyPress={handleEnterKeyPress}
+        handleEnterKeyPress={handleEnterKeyPress('taskName')}
       />
       <CardList />
     </>
   );
-};
+});
 
 export default TodoList;
 
