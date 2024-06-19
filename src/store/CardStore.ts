bdcd2d52f09954fa5e36a20732c07067a17d436b
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2024-06-19 19:23:07 +0900

    feat: CardStore에서 일정 완료, 북마크 기능 구현

--- src/store/CardStore.ts
+++ src/store/CardStore.ts
@@ -2,18 +2,9 @@ import { CardModel } from '@/models/CardModel';
 import { makeAutoObservable } from 'mobx';
 
 class CardStore {
-  private _card: CardModel;
   private _cardList: CardModel[];
 
   constructor() {
-    this._card = {
-      id: 0,
-      checked: false,
-      title: '',
-      date: '',
-      isBookMakred: false,
-      tagColor: '',
-    };
     this._cardList = [];
     makeAutoObservable(this);
   }
@@ -32,6 +23,28 @@ class CardStore {
 
   deleteCard(id: number) {
     this._cardList = this._cardList.filter(card => card.id !== id);
+  }
+
+  toggleChecked(id: number, checked: boolean) {
+    this._cardList = this._cardList.map(card =>
+      card.id === id
+        ? {
+            ...card,
+            checked,
+          }
+        : card,
+    );
+  }
+
+  toggleBookMark(id: number) {
+    this._cardList = this._cardList.map(card =>
+      card.id === id
+        ? {
+            ...card,
+            isBookMakred: !card.isBookMakred,
+          }
+        : card,
+    );
   }
 }
 
