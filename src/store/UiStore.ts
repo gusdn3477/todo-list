63038ce4696368823171f368f68a5ec82ee42ac2
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2024-06-20 16:37:04 +0900

    feat: 캘린더 관련한 전역으로 관리할 상태 및 함수 Ui Store에 추가

--- src/store/UiStore.ts
+++ src/store/UiStore.ts
@@ -2,9 +2,20 @@ import { makeAutoObservable } from 'mobx';
 
 class UiStore {
   private _open: boolean;
+  private _calendarOpen: boolean;
+  // 달력 위치에 대한 좌표
+  private _calendarPosition: {
+    top: number;
+    left: number;
+  };
 
   constructor() {
     this._open = false;
+    this._calendarOpen = false;
+    this._calendarPosition = {
+      top: 0,
+      left: 0,
+    };
     makeAutoObservable(this);
   }
 
@@ -12,8 +23,24 @@ class UiStore {
     this._open = status;
   }
 
+  handleCalendarVisible(status: boolean) {
+    this._calendarOpen = status;
+  }
+
+  handleCalendarPosition(top: number, left: number) {
+    this._calendarPosition = { top, left };
+  }
+
   get open() {
     return this._open;
+  }
+
+  get calendarOpen() {
+    return this._calendarOpen;
+  }
+
+  get calendarPosition() {
+    return this._calendarPosition;
   }
 }
 
