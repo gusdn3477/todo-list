6b5b410e21936fab72968668ef4588c400cb097b
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2024-06-20 20:08:30 +0900

    feat: 라우팅 기본 설정 및 기본 url인 경우 보여줄 페이지 구조 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TodoList from './pages/TodoList';
+import TodoCreate from './pages/TodoCreate';
 
 function App() {
-  return <TodoList />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoCreate />} />
+        <Route path=":id" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/TodoCreate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoCreate = () => {
+  return <StyledWrapper></StyledWrapper>;
+};
+
+export default TodoCreate;
+
+export const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
